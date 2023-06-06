5dcdb7b4964c9402d1fe711f58613e8c8fe777c8
Glados1998 <j.greder@staro.ch>
2023-06-06 23:52:50 +0200

    feat: init push project

---
+++ my_restaurent/src/test/java/com/example/my_restaurent/MyRestaurentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_restaurent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestaurentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
